13517cc53fb5df19a6804dca058b8589d49986c0
Srikantha98 <119388486+Srikantha98@users.noreply.github.com>
2025-01-25 13:20:50 +0000

    s

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,28 +1,21 @@
- 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-async function connectDB() {
+async function connectDB(){
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI)
 
-        const connection = mongoose.connection;
+        const connection = mongoose.connection
 
-        connection.on('connected', () => {
-            console.log('Successfully connected to MongoDB!');
-        });
+        connection.on('connected',()=>{
+            console.log("Connect to DB")
+        })
 
-        connection.on('error', (error) => {
-            console.error('MongoDB connection error:', error);
-        });
-
-        connection.on('disconnected', () => {
-            console.log('MongoDB connection disconnected!');
-        });
-        
+        connection.on('error',(error)=>{
+            console.log("Something is wrong in mongodb ",error)
+        })
     } catch (error) {
-        console.error('Error while connecting to MongoDB:', error);
-        process.exit(1); // Exit the process with a failure status
+        console.log("Something is wrong ",error)
     }
 }
 
-module.exports = connectDB;
+module.exports = connectDB
